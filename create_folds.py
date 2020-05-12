62c910e5efeed772439892a74bbd33e6a825fb02
kirill-kravtsov <alterkirill2@gmail.com>
2020-05-12 20:05:54 +0200

    initial commit

---
+++ create_folds.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/train.csv")
+    df = df.dropna().reset_index(drop=True)
+    df["fold"] = -1
+
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    for fold, (trn_, val_) in enumerate(kf.split(X=df, y=df.sentiment.values)):
+        print(len(trn_), len(val_))
+        df.loc[val_, 'fold'] = fold
+
+    df.to_csv("data/train_stratified_5folds.csv", index=False)
